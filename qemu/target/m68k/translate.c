2740114ed6cd0c63292f8bfd404ab1afbee17f45
Laurent Vivier <laurent@vivier.eu>
2018-03-15 16:22:29 -0400

    target/m68k: implement fatanh
    Using a local m68k floatx80_atanh()
    [copied from previous:
    Written by Andreas Grabher for Previous, NeXT Computer Emulator.]
    
    Backports commit e3655afa137b2e0999537eef273a2845ba21d68c from qemu

--- qemu/target/m68k/translate.c
+++ qemu/target/m68k/translate.c
@@ -5297,6 +5297,9 @@ DISAS_INSN(fpu)
     case 0x0c: /* fasin */
         gen_helper_fasin(tcg_ctx, uc->cpu_env, cpu_dest, cpu_src);
         break;
+    case 0x0d: /* fatanh */
+        gen_helper_fatanh(tcg_ctx, uc->cpu_env, cpu_dest, cpu_src);
+        break;
     case 0x0e: /* fsin */
         gen_helper_fsin(tcg_ctx, uc->cpu_env, cpu_dest, cpu_src);
         break;
